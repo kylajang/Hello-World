6cbc83e6fae8ffcc8bd15f4d0841d410ad7b2afd
kylajang <78120700+kylajang@users.noreply.github.com>
2022-02-28 16:27:35 -0800

    Create lab1.cpp

---
+++ lab1.cpp
@@ -0,0 +1,14 @@
+#include <isostream>
+
+        using std::cout;
+        using std::endl;
+
+int main()
+{
+        cout << "Hello, World!" << endl;
+        cout << "Kyla Jang" << endl;
+        cout << "888078243" << endl;
+        cout << "kylajang@csu.fullerton.edu" << endl;
+    return 0;
+}
+
